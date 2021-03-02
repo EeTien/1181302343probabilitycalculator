ca65f25e83f8dfdadf5db25840f84a4fad2a40d0
Lim Ee Tien <41426058+EeTien@users.noreply.github.com>
2021-03-02 18:06:03 +0800

    Add files via upload

---
+++ datadic.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def datadic():
+    st.title("Data dictionary")
+
+    st.header("Loan")
+    st.subheader("Term")
+    st.write("0: Long term  1: Short term")
+    st.subheader("Years in current job")
+    st.write("0: 1 year 1: 10+ years")
+    st.write("Other years follow its value")
+    st.subheader("Home Ownership")
+    st.write("0: Home Mortgage  1: Own Home    2: Rent")
